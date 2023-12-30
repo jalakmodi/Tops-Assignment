4506ccbd33f5e605fdc79453df66d81ae4e467c9
[200~jalakmodigit config --global user.email "jalakmodi85@gmail.com"git config --global user.name [200~ <jalakmodi85@gmail.com>
2023-12-30 10:53:07 +0530

    ghj

---
+++ pythonProject1/Assignment/Positive Negitive.py
@@ -0,0 +1,5 @@
+n=int(input("Enter Your Number : - "))
+if n<0:
+    print(f'Your Number {n} is Negative')
+else:
+    print(f'Your Number {n} is Positive')
---
+++ pythonProject1/Assignment/length String.py
@@ -0,0 +1,3 @@
+l=input("Enter Your Name : - ")
+la= len(l)
+print(la)
