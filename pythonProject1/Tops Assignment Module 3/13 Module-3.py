140862706e4df58a099d232bb0574841d442e0b4
[200~jalakmodigit config --global user.email "jalakmodi85@gmail.com"git config --global user.name [200~ <jalakmodi85@gmail.com>
2024-01-25 16:50:03 +0530

    31-12

--- pythonProject1/Tops Assignment Module 3/13 Module-3.py
+++ pythonProject1/Tops Assignment Module 3/13 Module-3.py
@@ -1,1 +1,6 @@
-#**************Replace Last Value of tuples in list***************
+#**************Find Repet Item In tupel***************
+t=(10,20,30,40,40,10,50)
+t1=()
+for i in t:
+    if t.count(i) > 1:
+        print(i)
